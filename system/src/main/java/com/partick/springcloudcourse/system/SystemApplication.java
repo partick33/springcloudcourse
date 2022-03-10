811193c098b42135d49668685f5bcd48da4fd650
patrick <775210145@qq.com>
2022-03-10 15:16:36 +0800

    1.将system注册到eureka

--- system/src/main/java/com/partick/springcloudcourse/system/SystemApplication.java
+++ system/src/main/java/com/partick/springcloudcourse/system/SystemApplication.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author partick_peng
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
