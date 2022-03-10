6ffea9a442a68ca0e6201820554a899983836005
patrick <775210145@qq.com>
2022-03-10 22:17:24 +0800

    1.增加business模块

---
+++ business/src/main/java/com/partick/springcloudcourse/system/BusinessApplication.java
@@ -0,0 +1,26 @@
+package com.partick.springcloudcourse.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author partick_peng
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(BusinessApplication.class);
+        ConfigurableEnvironment environment = application.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("Business地址: \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+
+}
