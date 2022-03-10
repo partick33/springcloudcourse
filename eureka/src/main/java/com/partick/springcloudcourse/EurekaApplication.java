03c732fd2e5af6cb68dabb026a3386bc3dad2ec1
patrick <775210145@qq.com>
2022-03-10 11:00:51 +0800

    增加Eureka子模块作为注册服务端

---
+++ eureka/src/main/java/com/partick/springcloudcourse/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.partick.springcloudcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author partick_peng
+ */
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
